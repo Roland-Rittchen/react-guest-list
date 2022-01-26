93e9ad3fefd8842737de927a2704647d286876ef
Roland Rittchen <rrittchen@gmail.com>
2022-01-26 20:08:28 +0100

    Finish mockup

--- src/index.js
+++ src/index.js
@@ -11,11 +11,56 @@ ReactDOM.render(
         *,
         ::after,
         ::before {
-          color: #413d45;
+          color: white;
           box-sizing: border-box;
           margin: 0;
-          font-family: 'Apercu', sans-serif;
-          letter-spacing: -0.01em;
+          font-family: sans-serif;
+        }
+
+        li {
+          padding: 10px;
+          background-color: #53555e;
+          list-style-type: none;
+          margin: 12px;
+          border-radius: 8px;
+        }
+
+        label {
+          margin: 4px 8px;
+          text-align: center;
+        }
+
+        button {
+          background-color: #4e86bd;
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          margin: 4px 2px;
+          cursor: pointer;
+          border-radius: 4px;
+          transition-duration: 0.4s;
+        }
+        button:hover {
+          background-color: #a6c2de;
+        }
+        button:active {
+          background-color: #a6c2de;
+          color: rgba(40, 41, 54, 1);
+        }
+        input {
+          color: white;
+          background-color: #919298;
+          border: 2px #a6c2de;
+          border-radius: 4px;
+          padding: 12px 20px;
+          margin: 8px 8px;
+        }
+        input:focus {
+          background-color: #a6c2de;
+          color: rgba(40, 41, 54, 1);
         }
       `}
     />
