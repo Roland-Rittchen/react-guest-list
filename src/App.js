01a661a809744e1a52dc2e7cb45375e12bc1d81e
Roland Rittchen <rrittchen@gmail.com>
2022-01-26 18:52:15 +0100

    Setup Emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const appOutline = css`
+  background-color: rgba(40, 41, 54, 1);
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
+      <div css={appOutline}>Guest List</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        *,
+        ::after,
+        ::before {
+          color: #413d45;
+          box-sizing: border-box;
+          margin: 0;
+          font-family: 'Apercu', sans-serif;
+          letter-spacing: -0.01em;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
